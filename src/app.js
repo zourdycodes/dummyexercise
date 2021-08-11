e69edc997d4f0dfe498061e65aa1f7eb24e2831f
Muhammad Zourdy <zhafarmuhammad12@gmail.com>
2021-08-11 20:55:47 +0700

    changing the application title

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ import "./global/App.css";
 export const App = () => {
   return (
     <div className="App">
-      <h2 className="title">Food Search</h2>
+      <h2 className="title">Healthy Food Search</h2>
 
       <SearchInput />
       <SearchResult />
